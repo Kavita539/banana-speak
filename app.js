a89c7db3c74723bfd7084f1a2952a7f68887304d
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2021-08-20 20:08:56 +0530

    updated script

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var txtInput = document.querySelector("#txtInput");
+var btnTranslate = document.querySelector("#btnTranslate");
+var divOutput = document.querySelector("#output");
 
-function getTranslationURL(input) {
-    return serverURL + "?" + "text=" + input
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+//this function will append the user input with the api url and return 
+function getTranslationUrl(text)
+{
+    return serverUrl + "?text=" + text;
 }
 
-function errorHandler(error) {
-    console.log("error occured", error);
-    alert("something wrong with server! try again after some time")
+//this function will show alert to user if any error occurs while fetching from api
+function errorHandler(error)
+{
+    console.log(error);
+    alert("Something is wrong with the server! Please try after sometime");
 }
 
+//this is what will happen when translate button is clicked - function of translate button
+function clickHandlerOfTranslate()
+{
+    var inputValue = txtInput.value; //taking input value from textarea input box
+    fetch(getTranslationUrl(inputValue)).then(Response => Response.json()).then(json => divOutput.textContent = json.contents.translated).catch(errorHandler);
+}
 
-function clickHandler() {
-    var inputText = txtInput.value; // taking input
-
-    // calling server for processing
-    fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; // output
-           })
-        .catch(errorHandler)
-};
-
-btnTranslate.addEventListener("click", clickHandler)
+//attched the event and event handler with the button
+btnTranslate.addEventListener("click", clickHandlerOfTranslate);
